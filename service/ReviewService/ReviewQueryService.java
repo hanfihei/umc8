878c8e748df3870d2aabd505c9804ce4cbb8381f
jihei <84708348+hanfihei@users.noreply.github.com>
2025-05-25 19:58:59 +0900

    Delete service directory

--- service/ReviewService/ReviewQueryService.java
+++
@@ -1,9 +0,0 @@
-package umc.spring.study.service.ReviewService;
-
-import org.springframework.data.domain.Page;
-import umc.spring.study.domain.Review;
-
-public interface ReviewQueryService {
-    void insertReview(String text, double star, Long userId, Long storeId);
-    Page<Review> getReviewsByUser(Long userId, int page);
-}
