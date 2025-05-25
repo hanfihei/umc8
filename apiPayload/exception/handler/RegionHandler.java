b821f23cbd5164bc18860788bc544b870e037fb0
jihei <84708348+hanfihei@users.noreply.github.com>
2025-05-25 19:55:15 +0900

    Delete apiPayload directory

--- apiPayload/exception/handler/RegionHandler.java
+++
@@ -1,9 +0,0 @@
-package umc.spring.study.apiPayload.exception.handler;
-
-import umc.spring.study.apiPayload.code.BaseErrorCode;
-
-public class RegionHandler extends GeneralException {
-    public RegionHandler(BaseErrorCode errorCode) {
-        super(errorCode);
-    }
-}
